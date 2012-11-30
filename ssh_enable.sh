40f747a063877b58e8d4b07d843e45ab4c75a055
Tyson Reimer <tyreimer@gmail.com>
2012-11-29 19:47:59 -0500

    Enable SSH
    This script was not written by me and any credit should go to the
    author.
    
    This script can be used assuming that the machine has a TechSupport
    Admin account.  Otherwise the account name will need to be changed on
    line 30.

---
+++ ssh_enable.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# eccs_config_ssh
+# Patrick Gallagher
+# Created 2/16/2012
+# Modified 3/27/2012
+
+## Change Log
+# 2/16/2012 - Initial script
+# 3/1/2012 - Added logic to create ssh group only if it didn't already exist. 
+# 3/5/2012 - Fixed logic with help of @gregneagle and @tvsutton
+# 3/27/2012 - Changed how ssh is enabled. No longer using deprecated systemsetup. 
+
+# Enable ssh
+/usr/libexec/PlistBuddy -c "Delete Disabled" "/System/Library/LaunchDaemons/ssh.plist"
+launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+
+# Create the com.apple.access_ssh group
+dscl . read /Groups/com.apple.access_ssh > /dev/null 2>&1
+if [ "$?" != "0" ]; then
+	echo "Creating ssh access group"
+	dseditgroup -o create -q com.apple.access_ssh
+fi
+
+# Add the admin group to com.apple.access_ssh
+dseditgroup -o edit -a admin -t group com.apple.access_ssh
+
+# Add our admin acct to group. This is mainly to remain consistent from what we did the past. 
+# Otherwise not needed. 
+dseditgroup -o edit -a techsupport -t user com.apple.access_ssh
+
+exit 0
